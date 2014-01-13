fa2013bf3597be82a40603d3a4689f1f027d8676
Dmitry Mozzherin <dmozzherin@gmail.com>
2014-01-13 16:57:47 -0500

    multiple graph

--- routes.rb
+++ routes.rb
@@ -75,10 +75,27 @@ class SeabaseApp < Sinatra::Base
     @row3 = ['combined',4621,5321,5131,11621,3980,6818,3256,4597,11200,
             7030,12841,16216,19018,14440,17749,23066,26699,14501,19732,18041]
 
-    @combined_data = get_combined(@row3) 
+    @combined_data = get_combined(@row3.dup) 
+    @separate_data = get_separate(@row1, @row2, @row3)
     haml :google_chart
   end
- 
+
+  def get_separate(r1, r2, r3)
+    count = 0
+    res = [['Hour', r1.shift, r2.shift, r3.shift ]]
+    until r1.empty? do
+      d1 = r1.shift.to_i
+      d2 = r2.shift.to_i
+      d3 = r3.shift.to_i
+      d1 = nil if d1 == 0
+      d2 = nil if d2 == 0
+      d3 = nil if d3 == 0
+      res << [count, d1, d2, d3]
+      count+=1
+    end
+    res.to_json.gsub(/null/, '')
+  end
+
   def get_combined(row)
     count = 0
     res = [['Hour', row.shift, {type: 'string', role: 'tooltip'} ]]
@@ -89,5 +106,6 @@ class SeabaseApp < Sinatra::Base
     end
     res
   end
+
 end
 
