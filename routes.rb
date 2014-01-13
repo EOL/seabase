2f41c5f8dc3a26a93cdf20e5bd94d21b36a7261a
Dmitry Mozzherin <dmozzherin@gmail.com>
2014-01-13 15:07:24 -0500

    graph for combination

--- routes.rb
+++ routes.rb
@@ -65,6 +65,29 @@ class SeabaseApp < Sinatra::Base
     @en = ExternalName.find(params[:id])
     haml :external_name
   end
-  
+
+  get '/graph_test' do 
+    @name = 'A0PJN4'
+    @row1 = ['A',6161,3750,6097,0,0,6818,3256,0,11200,7030,12841,19105,
+            17866,13156,16136,21079,25991,14865,19888,14275]
+    @row2 = ['B',3082,6891,4166,11621,3980,0,0,4597,0,0,0,13326,20170,
+            15724,19362,25053,27408,14136,19576,21808]
+    @row3 = ['combined',4621,5321,5131,11621,3980,6818,3256,4597,11200,
+            7030,12841,16216,19018,14440,17749,23066,26699,14501,19732,18041]
+
+    @combined_data = get_combined(@row3) 
+    haml :google_chart
+  end
+ 
+  def get_combined(row)
+    count = 0
+    res = [['Hour', row.shift, {type: 'string', role: 'tooltip'} ]]
+    until row.empty? do
+      datum = row.shift
+      res << [count, datum, "%s h\n%s amol/embr." % [count, datum]] if datum.to_i != 0
+      count += 1
+    end
+    res
+  end
 end
 
