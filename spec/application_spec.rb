998ee56c810d50a62b27147ac4e8d8f0c79a2a45
Dmitry Mozzherin <dmozzherin@gmail.com>
2014-01-22 13:49:15 -0500

    gui search specs

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -22,24 +22,45 @@ describe SeabaseApp do
       expect(page.body).to match %q(For example 'polymerase alpha', 'P17918')
     end
 
+    context 'html' do
+      it 'returns html page' do
+        data = [{ term: 'sox',  count: 3 },
+                { term: 'Transcription', count: 2},
+                { term: 'O95416', count: 1 }]
+        data.each do |datum|
+          visit('/search?scientific_name=Homo+sapiens'\
+                "&term=%s&exact_search=false" % datum[:term])
+          expect(page.status_code).to eq 200
+          if datum[:count] > 1
+            expect(page.body).to match "Found %s items" % datum[:count]
+          else
+            expect(page.body).not_to match "Found %s items" % datum[:count]
+            expect(page.body).to match /homologous.*ortholog SOX14/m
+          end
+        end
+      end
+    end
+
     context 'json' do
       it 'returns results in json' do
-        visit('/search.json?scientific_name=Homo+sapiens'\
-              '&term=sox&exact_search=false')
-        res = JSON.parse(page.body, symbolize_names: true)
-        expect(res.map { |r| r[:gene_name] }.sort).
-          to eq ['QSOX1', 'SOX14', 'SOX18']
-        visit('/search.json?scientific_name=Homo+sapiens'\
-              '&term=Transcription&exact_search=false')
-        res = JSON.parse(page.body, symbolize_names: true)
-        expect(res.map { |r| r[:functional_name] }.sort).
-          to eq  ['Transcription factor SOX-14', 'Transcription factor SOX-18']
-        visit('/search.json?scientific_name=Homo+sapiens'\
-              '&term=O95416&exact_search=false')
-        res = JSON.parse(page.body, symbolize_names: true)
-        expect(res.map { |r| r[:name] }.sort).
-          to eq  ['O95416']
+        data = [{ term: 'sox', result: ['QSOX1', 'SOX14', 'SOX18'], 
+                  field: :gene_name},
+                { term: 'Transcription', result: 
+                  ['Transcription factor SOX-14', 
+                   'Transcription factor SOX-18'], 
+                   field: :functional_name },
+                { term: 'O95416', result: ['O95416'],
+                   field: :name }]
+
+        data.each do |datum|
+          visit('/search.json?scientific_name=Homo+sapiens'\
+                "&term=%s&exact_search=false" % datum[:term])
+          res = JSON.parse(page.body, symbolize_names: true)
+          expect(res.map { |d| d[datum[:field]] }.sort).
+            to eq datum[:result]
+        end
       end
+
     end
   end
 
