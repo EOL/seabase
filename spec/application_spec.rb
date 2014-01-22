699f1c9421bcca236da048c9c27667100f571932
Dmitry Mozzherin <dmozzherin@gmail.com>
2014-01-22 12:19:34 -0500

    gui tests

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -6,6 +6,31 @@ describe SeabaseApp do
       expect(page.body).to match 'SeaBase'
     end
   end
+
+  describe '/blast' do
+    it 'renders' do 
+      visit '/blast'
+      expect(page.status_code).to eq 200
+      expect(page.body).to match 'Blast'
+    end
+  end
+
+  describe '/search' do
+    it 'renders' do
+      visit '/search'
+      expect(page.status_code).to eq 200
+      expect(page.body).to match %q(For example 'polymerase alpha', 'P17918')
+    end
+  end
+
+  describe '/external_names' do
+    it 'renders' do
+      visit '/external_names/25360'
+      expect(page.status_code).to eq 200
+      expect(page.body).to match 'the human ortholog QSOX1'
+    end
+  end
+  
 end
 
 
--- spec/lib/blast_spec.rb
+++ spec/lib/blast_spec.rb
@@ -20,8 +20,10 @@ describe Seabase::Blast do
     let(:seq) { File.read(File.expand_path('../../files/pcna_fasta.txt',
                                            __FILE__)) }
     it 'finds sequence' do
-      res = blast.search(seq)
-      expect(res).to be_kind_of Bio::Blast::Report
+      unless RUBY_PLATFORM =~ /darvin/
+        res = blast.search(seq)
+        expect(res).to be_kind_of Bio::Blast::Report
+      end
     end
   end
 
